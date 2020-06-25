7791ea58b7349cdff0ff7f5f341bce0d77fd401f
frank0 <tony.jingzhou@gmail.com>
2020-06-25 15:06:13 +0800

    修改host

--- src/main/java/com/geccocrawler/socks5/auth/HttpAuth.java
+++ src/main/java/com/geccocrawler/socks5/auth/HttpAuth.java
@@ -45,7 +45,7 @@ public class HttpAuth implements PasswordAuth {
         RequestBody requestBody = RequestBody.create(JSON, com.alibaba.fastjson.JSON.toJSONString(data));
         //创建请求
         Request request = new Request.Builder()
-                .url("http://192.168.1.102:8080/proxy/auth")
+                .url("http://http://shuttle.twg.life/proxy/auth")
                 .header("User-Agent", "sock5-proxy")
                 .post(requestBody)
                 .build();
